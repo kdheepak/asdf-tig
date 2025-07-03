933348d70ccc05bc86dd201b1a55dfe520501946
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-07-03 09:55:07 -0400

    chore: Lint

--- lib/utils.bash
+++ lib/utils.bash
@@ -5,7 +5,6 @@ set -euo pipefail
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for tig.
 GH_REPO="https://github.com/jonas/tig"
 TOOL_NAME="tig"
-TOOL_TEST="tig --help"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
